a79c5379136c985d789e8910f59ec432daeddc10
Avinoam Henig <avhenig@gmail.com>
2016-06-30 11:05:02 -0400

    Update ast spec tentatively to include types and data constructors

--- app/ast/ast-spec.js
+++ app/ast/ast-spec.js
@@ -1,32 +1,68 @@
-const VERSION = 2.2;
+const VERSION = 3.0;
 
 const Program = {
-  astType     : TYPE_NAME,
-  expression  : Uid(Expression),
-  nodes       : {[Uid]:Node},
-  identifiers : {[Uid]:Identifier}
+  astType         : TYPE_NAME,
+  expression      : Uid(Expression),
+  nodes           : {[Uid]:Node},
+  identifiers     : {[Uid]:Identifier},
+  constructors    : {[Uid]:Constructor},
+  typeDefinitions : {[Uid]:TypeDefinition},
+  typeVariables   : {[Uid]:TypeVariable}
 };
 
 const ProgramFragment = {
-  astType     : TYPE_NAME,
-  rootNode    : Uid(Node),
-  nodes       : {[Uid]:Node},
-  identifiers : {[Uid]:Identifier}
+  astType         : TYPE_NAME,
+  rootNode        : Uid(Node),
+  nodes           : {[Uid]:Node},
+  identifiers     : {[Uid]:Identifier},
+  constructors    : {[Uid]:Constructor},
+  typeDefinitions : {[Uid]:TypeDefinition},
+  typeVariables   : {[Uid]:TypeVariable}
 };
 
 const Identifier = {
   astType     : TypeName,
   id          : NewUid(),
-  displayName : nullable(String),
+  displayName : String,
   scope       : nullable(Uid(Node)),
   value       : nullable(Uid(Expression))
 };
 
+const Constructor = {
+  astType       : TypeName,
+  id            : NewUid(),
+  displayName   : String,
+  argumentTypes : [Uid(Type)],
+  parentType    : Uid(TypeDefinition)
+};
+
+const TypeDefinition = {
+  astType      : TypeName,
+  id           : NewUid(),
+  displayName  : String,
+  constructors : nonempty([Uid(Constructor)]),
+  arguments    : [Uid(TypeVariable)]
+};
+
+const TypeVariable = {
+  astType     : TypeName,
+  id          : NewUid(),
+  displayName : nullable(String)
+};
+
+const Type = {
+  astType        : TypeName,
+  id             : NewUid(),
+  displayName    : nullable(String),
+  typeDefinition : Uid(any(TypeDefinition, TypeVariable)),
+  arguments      : [Type]
+};
+
 const Node = {
-  nodeType          : noOverride(TypeName),
-  id                : NewUid,
-  parent            : nullable(Uid(Node)),
-  displayName       : nullable(String),
+  nodeType         : noOverride(TypeName),
+  id               : NewUid,
+  parent           : nullable(Uid(Node)),
+  displayName      : nullable(String),
   boundIdentifiers : [Uid(Node)],
   ...sub(
     Expression,
@@ -39,6 +75,7 @@ const Expression = {
   ...extend(Node),
   nodeType       : noOverride(TypeName),
   expressionType : TypeName,
+  type           : Type,
   ...sub(
     NumberExpression,
     IdentifierExpression,
@@ -85,4 +122,14 @@ const CaseBranch = {
 const ElseBranch = {
   ...extend(Node),
   expression : Uid(Expression)
+};
+
+const ConstructionExpression = {
+  ...extend(Expression),
+  constructor : Uid(Constructor),
+  arguments   : [Uid(Expression)]
+};
+
+const DefaultExpression = {
+  ...extend(Expression)
 };
